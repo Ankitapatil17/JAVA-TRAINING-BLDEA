3869ef906d77b479de10994faa72b4fbbdbdf917
Ankita Patil <Ankita Patil@LAPTOP-HF5EL8Q7>
2023-03-27 16:59:21 +0530

    added commit

---
+++ src/main/java/trainingcodes/ArrayCode1.java
@@ -0,0 +1,33 @@
+/**
+ * 
+ */
+package array;
+
+/**
+ * @author Ankita Patil
+ *
+ */
+import java.util.Scanner;
+public class ArrayCode1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Enter the count of students:");
+		Scanner sc = new Scanner(System.in);
+		int n=sc.nextInt();
+		
+		int arr[]=new int[n];
+		for(int i=0;i<n;i++) {
+			System.out.println("Enter the marks of students number"+(i+1));
+			arr[i]=sc.nextInt();
+		}
+		for(int i=0;i<n;i++) {
+			System.out.println("The marks of student number"+(i+1)+"is:"+arr[i]);
+		}
+
+	}
+
+}
