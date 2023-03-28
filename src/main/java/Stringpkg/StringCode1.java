43631643b54d1b2e5d19ed9a9faad3c098f03b6e
Ankita Patil <Ankita Patil@LAPTOP-HF5EL8Q7>
2023-03-28 12:15:25 +0530

    Stringpkg

---
+++ src/main/java/Stringpkg/StringCode1.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package Stringpkg;
+
+/**
+ * @author Ankita Patil
+ *
+ */
+public class StringCode1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/main/java/Stringpkg/StringCode2.java
@@ -0,0 +1,40 @@
+
+/**
+ * 
+ */
+package Stringpkg;
+
+/**
+ * @author Ankita Patil
+ *
+ */
+public class StringCode2 {
+
+	/**
+	 * @param args
+	 */
+	
+			public static void main(String[] args) {
+				// TODO Auto-generated method stub
+				String str="RajaRamMohanRoy";
+				System.out.println(str.length());
+				System.out.println(str.toUpperCase());
+				System.out.println(str.toLowerCase());
+				System.out.println(str.charAt(1));
+				//system.out.println(str.charAt(120));
+				int a=10+str.charAt(1);
+				System.out.println(a);
+				System.out.println(str.indexOf('R'));
+				System.out.println(str.indexOf('Z'));
+				System.out.println(str.startsWith("Raja"));
+				System.out.println(str.startsWith("Roja"));
+				System.out.println(str.endsWith("Roy"));
+				System.out.println(str.endsWith("Boy"));	
+				System.out.println(str.contains("Mohan"));
+				System.out.println(str.contains("Rohan"));
+				System.out.println(str.contains("mohan"));
+
+
+			}
+
+		}
